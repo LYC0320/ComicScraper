65c3cb589c1b1632b27a062253ca0ec47881f389
LYC0320 <blackfield0320@gmail.com>
2019-04-02 17:05:57 +0800

    Fix long page bug

--- comicScraper.py
+++ comicScraper.py
@@ -4,19 +4,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import time
 from PIL import Image
 import os
 import xml.etree.ElementTree as ET
 import sys
+import re
 
 chrome_options = Options()
 chrome_options.add_argument("--headless") 
 
 urlHost = "https://tw.manhuagui.com"
 
-#user input
-urlPath = "/comic/13948/"
+# user input
+urlPath = "/comic/7580/"
 if len(sys.argv) > 1:
 	urlPath = sys.argv[1]
 
@@ -39,10 +39,10 @@ def getTitle():
 def downloadPicture():
 	driver = webdriver.Chrome(executable_path = "./WebDriver/chromedriver.exe", chrome_options = chrome_options)
 	driver.get(urlHost + urlPath)
-	epNum = len(driver.find_elements_by_class_name("status0"))
-	chapterTitles = []
-	chapterNums = []
+	blockList = driver.find_elements_by_css_selector("div[class = 'chapter-list cf mt10']")
+	blockNum = len(blockList)
 
+	# create newest xml
 	xmlPath = path + newestFile
 
 	if os.path.exists(xmlPath):
@@ -51,53 +51,69 @@ def downloadPicture():
 	else:
 		root = ET.Element("Newest")
 		title = ET.SubElement(root, "title")
+		title.text = "0"
 		tree = ET.ElementTree(root)
 		tree.write(xmlPath)
 
+	newestTitle = driver.find_element_by_css_selector("ul[style = 'display:block']").find_element_by_tag_name("a").get_attribute("title")
 
-	for ep in range(0, epNum):
-		chapter = driver.find_elements_by_class_name("status0")[ep]
-		chapterNum = chapter.find_element_by_tag_name("i").text
-		chapterNum = chapterNum[0 : len(chapterNum) - 1]
-		chapterNums.append(chapterNum)
-		chapterTitle = chapter.get_attribute("title")
-		chapterTitles.append(chapterTitle)
+	for block in range(0, blockNum):
+		epList = blockList[block].find_elements_by_tag_name("li")
+		epNum = len(epList)
 
-		if root.find("title").text == chapterTitle:
-			root.find("title").text = chapterTitles[0]
-			tree.write(xmlPath, "UTF-8")
-			break
-			
-		chapter.click()
+		# pagination
+		for page in blockList[block].find_elements_by_tag_name("ul"):
+			driver.execute_script("arguments[0].setAttribute('style' , 'display:block')", page)
 
-		for i in range(0, int(chapterNum)):
-			driver.switch_to.window(driver.window_handles[-1])
-			picUrl = driver.find_element_by_id("mangaFile").get_attribute("src")
-			req = urllib.request.Request(url = picUrl, headers = headers)
-			data = urllib.request.urlopen(req).read()
-			newDirectory = chapterTitle + "/"
-			fileName = path + newDirectory + chapterTitle + "_" + str(i + 1)
-			print(fileName)
-			if not os.path.exists(path + newDirectory):
-				os.makedirs(path + newDirectory)
+		lastNewest = root.find("title")
 
-			with open(fileName, "wb") as f:
-				f.write(data)
-				f.close()
+		for ep in range(0, epNum):
+			chapter = epList[ep].find_element_by_tag_name("a")
+			chapterTitle = chapter.get_attribute("title")
 
-			tmpImg = Image.open(fileName).convert("RGB")
-			tmpImg.save(fileName + jpgType,"jpeg")
-			os.remove(fileName)
-			
-			if i != int(chapterNum) - 1:
-				driver.find_element_by_link_text("下一頁").click()
-			else:
-				driver.find_element_by_link_text("目錄列表").click()
-				driver.switch_to.window(driver.window_handles[0])
+			# 去p
+			chapterNum = chapter.find_element_by_tag_name("i").text
+			chapterNum = chapterNum[0 : len(chapterNum) - 1]
 
-		if ep == epNum - 1:
-			root.find("title").text = chapterTitles[0]
-			tree.write(xmlPath, "UTF-8")
+			# update to the last newest
+			if lastNewest.text == chapterTitle:
+				break
+
+			chapter.click()
+
+			for i in range(0, int(chapterNum)):
+				driver.switch_to.window(driver.window_handles[-1])
+				picUrl = driver.find_element_by_id("mangaFile").get_attribute("src")
+				req = urllib.request.Request(url = picUrl, headers = headers)
+				data = urllib.request.urlopen(req).read()
+				newDirectory = chapterTitle + "/"
+				fileName = path + newDirectory + chapterTitle + "_" + str(i + 1)
+				print(fileName)
+				if not os.path.exists(path + newDirectory):
+					os.makedirs(path + newDirectory)
+				elif len(os.listdir(path + newDirectory)) == int(chapterNum):
+					driver.close()
+					driver.switch_to.window(driver.window_handles[0])
+					break
+
+				with open(fileName, "wb") as f:
+					f.write(data)
+					f.close()
+
+				tmpImg = Image.open(fileName).convert("RGB")
+				tmpImg.save(fileName + jpgType,"jpeg")
+				os.remove(fileName)
+				
+				if i != int(chapterNum) - 1:
+					driver.find_element_by_link_text("下一頁").click()
+				else:
+					driver.close()
+					driver.switch_to.window(driver.window_handles[0])
+
+	# update newest
+	lastNewest.text = newestTitle
+	tree.write(xmlPath, "UTF-8")
+		
 
 def updateCategory():
 	xmlPath = "./Category.xml"
@@ -123,11 +139,15 @@ def updateCategory():
 		tree = ET.ElementTree(root)
 		tree.write(xmlPath)
 
+def main():
+	if __name__ == "__main__":
 
-getTitle()
+		getTitle()
 
-downloadPicture()
+		downloadPicture()
 
-updateCategory()
+		updateCategory()
 
-#bug:/comic/27937/
+main()
+
+# bug:/comic/27937/
